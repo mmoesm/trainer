ba90aef41d17b6eefbb249a10d2c0b8d9c4900f0
mmoesm <42961608+mmoesm@users.noreply.github.com>
2020-11-25 11:57:19 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-unzip datasets.npz
-pip3 install virtualenv
+unzip datasets.zip
+# Not necessary, already installed: pip3 install virtualenv 
 python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
